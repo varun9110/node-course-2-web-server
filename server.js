477f1f2d957acbff0e96d489a8d057473c6ec03b
Goel <varun.goel@sap.com>
2018-11-16 03:55:05 +0530

    Add project page

--- server.js
+++ server.js
@@ -62,6 +62,12 @@ app.get('/about', (req , res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects'
+  });
+});
+
 app.get('/bad' , (req , res) => {
   res.send({
     errorMessgae : 'Unable to handle request'
